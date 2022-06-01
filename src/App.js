58b9d438ea61ef7ee29844d19a084fae693093cc
kourdis <kourdismohamed3@gmail.com>
2022-06-01 19:15:55 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <span>ok</span>
+      <h1>this is a f change</h1>
     </div>
   );
 }
---
+++ src/operations/userChoice.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const UserChoice = (props) => {
+    return ( 
+        <div className='d-flex justify-content-between'>
+            <div>
+                <span>somthing did change</span>
+            </div>
+        </div>
+     );
+}
+ 
+export default UserChoice;
